243fa05c8cd6ff843f1bc51c94b2de3210d44aa8
ririyeye <200610237@qq.com>
2020-12-21 14:29:38 +0800

    ec200 init

--- USER/RTE/_TEST/RTE_Components.h
+++ USER/RTE/_TEST/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Configuration File
+ * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'TEST' 
@@ -16,12 +16,9 @@
  */
 #define CMSIS_device_header "stm32f10x.h"
 
-/*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.1 */
 #define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
         #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
-/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
           #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
-
 
 #endif /* RTE_COMPONENTS_H */
